c033280d526362fbeef781c214b8b647afa856d3
Claude Fitz <claudkuate@gmail.com>
2023-06-10 19:39:16 +0100

    iniatial commit

---
+++ src/post/post.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+  create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({data:createPostDto});
+  }
+
+  findAll() {
+    return this.prisma.post.findMany();
+  }
+  
+  findAllByCollectionId(id: string) {
+    return this.prisma.post.findMany({where:{collectionId: id}});
+  }
+  
+  findAllByProfileId(id: string) {
+    return this.prisma.post.findMany({where:{collection:{profileId: id}}});
+  }
+
+  findOne(id: string) {
+    return this.prisma.post.findUnique({where:{id:id}});
+  }
+
+  update(id: string, updatePostDto: UpdatePostDto) {
+    return this.prisma.post.update({data:updatePostDto, where:{id: id}});
+  }
+
+  remove(id: string) {
+    return this.prisma.post.delete({where:{id:id}});
+  }
+}
